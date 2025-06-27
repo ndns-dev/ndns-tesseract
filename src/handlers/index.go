6d2eee9a140598f32e8574111c99b24979aefe4d
sh5080 <prpn97@gmail.com>
2025-06-27 16:58:13 +0900

    fix: 이벤트 분기 방식 수정

--- src/handlers/index.go
+++ src/handlers/index.go
@@ -3,37 +3,28 @@ package handlers
 import (
 	"context"
 	"encoding/json"
-	"fmt"
+	"log"
 
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/ndns-dev/ndns-tesseract/src/utils"
 )
 
-// HandleRequest는 Lambda 함수의 메인 핸들러입니다.
-func HandleRequest(ctx context.Context, event interface{}) (interface{}, error) {
-	eventBytes, err := json.Marshal(event)
-	if err != nil {
-		return utils.Response(utils.ErrorHandler(ctx, fmt.Errorf("failed to marshal event: %w", err), "", "", "EventMarshal"))
-	}
-
-	var eventMap map[string]interface{}
-	if err := json.Unmarshal(eventBytes, &eventMap); err == nil {
-		// API Gateway 이벤트인지 확인
-		if _, hasBody := eventMap["body"]; hasBody {
-			if _, hasHeaders := eventMap["headers"]; hasHeaders {
-				// API Gateway 이벤트로 처리
-				var apiEvent events.APIGatewayProxyRequest
-				if err := json.Unmarshal(eventBytes, &apiEvent); err == nil {
-					return HandleAPIGatewayEvent(ctx, apiEvent)
-				}
-			}
+func HandleRequest(ctx context.Context, event json.RawMessage) (interface{}, error) {
+	// SQS 이벤트 체크
+	var sqsEvent events.SQSEvent
+	if err := json.Unmarshal(event, &sqsEvent); err == nil {
+		if len(sqsEvent.Records) > 0 && sqsEvent.Records[0].EventSource == "aws:sqs" {
+			return HandleSQSEvent(ctx, sqsEvent)
 		}
 	}
 
-	// SQS 이벤트 처리
-	if sqsEvent, ok := event.(events.SQSEvent); ok {
-		return HandleSQSEvent(ctx, sqsEvent)
+	// API Gateway 이벤트 체크
+	var apiEvent events.APIGatewayProxyRequest
+	if err := json.Unmarshal(event, &apiEvent); err == nil {
+		if apiEvent.Body != "" {
+			return HandleAPIGatewayEvent(ctx, apiEvent)
+		}
 	}
 
-	return utils.Response(utils.ErrorHandler(ctx, fmt.Errorf("unsupported event type"), "", "", "EventTypeHandler"))
+	log.Printf("Unsupported event type: %s", string(event))
+	return nil, nil
 }
